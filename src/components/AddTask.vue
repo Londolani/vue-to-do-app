0cbf94f75ac1361c4b227c301946fa74551b375d
Londolani <londolani0700@gmail.com>
2023-07-20 08:26:29 +0200

    added some styles

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,25 +1,63 @@
 <template>
     <div>
-        <form @submit.prevent="submitData">
-            <label for="task"></label>
-            <input name="task" type="text" v-model="input">
-            <button>Add task</button>
-        </form>
+      <form class="task-form" @submit.prevent="submitData">
+        <label for="task">Task:</label>
+        <input name="task" type="text" v-model="input" placeholder="Enter your task here">
+        <button type="submit">Add Task</button>
+      </form>
     </div>
-</template>
-
-<script>
-export default{
-    //emits:['inputValue'],
-    data(){
-        return{
-            input:''
-        }
-    },methods:{
-        submitData(){
-            this.$emit('inputValue',this.input);
-            this.input ='';
-        }
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        input: ''
+      }
+    },
+    methods: {
+      submitData() {
+        this.$emit('inputValue', this.input);
+        this.input = '';
+      }
     }
-}
-</script>
+  }
+  </script>
+  
+  <style scoped>
+  .task-form {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+  }
+  
+  label {
+    font-weight: bold;
+    margin-right: 10px;
+  }
+  
+  input[type="text"] {
+    width: 60%;
+    padding: 10px 20px;
+    margin-right: 10px;
+    box-sizing: border-box;
+    border: 2px solid #4CAF50;
+    border-radius: 4px;
+  }
+  
+  button[type="submit"] {
+    padding: 10px 20px;
+    background-color: #4CAF50;
+    border: none;
+    color: white;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+  
+  button[type="submit"]:hover {
+    background-color: #45a049;
+  }
+  </style>
+  
--- src/components/ListTask.vue
+++ src/components/ListTask.vue
@@ -1,19 +1,72 @@
 <template>
     <div class="card">
-        <div class="card-header">
-            <p>{{ name }}</p>
-            <button @click="removeTask(id)">remove</button>
+      <div class="card-header">
+        <p>{{ name }}</p>
+        <div class="buttons">
+          <button class="edit-button" @click="editTask(id)">Edit</button>
+          <button class="remove-button" @click="removeTask(id)">Remove</button>
         </div>
+      </div>
     </div>
-</template>
-
-<script>
-export default {
-    props: ['name','id'],
+  </template>
+  
+  <script>
+  export default {
+    props: ['name', 'id'],
     methods: {
-        removeTask(id) {
-            this.$emit('removeTask', id);
-        },
+      editTask(id) {
+        this.$emit('editTask', id);
+      },
+      removeTask(id) {
+        this.$emit('removeTask', id);
+      },
     },
-};
-</script>
+  };
+  </script>
+  
+  <style scoped>
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-color:#4CAF50;
+  }
+  
+  .card-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  
+  p {
+    font-size: 18px;
+    margin: 0;
+    padding: 10px;
+  }
+  
+  .buttons {
+    display: flex;
+    align-items: center;
+  }
+  
+  button {
+    padding: 8px 16px;
+    margin-left: 8px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+  
+  .edit-button {
+    background-color: green;
+    color: #fff;
+  }
+  
+  .remove-button {
+    background-color: black;
+    color: #fff;
+  }
+  
+  </style>
+  
